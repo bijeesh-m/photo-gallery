870ef9aa2ef52ec93b58a619995e37afbcb74ff5
Bijeesh M <bijeeshh.m@gmail.com>
2025-01-13 15:09:41 +0530

    First commit

---
+++ Client/src/Component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+            <div className="footer mt-5" style={{ boxShadow: "-4px -4px 14px rgb(13,202,240)" }}>
+                <footer class="py-2 bg-dark text-center">Copyright @ 2024</footer>
+            </div>
+        </>
+    );
+};
+
+export default Footer;
---
+++ Client/src/Component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar bg-info">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <span className="fw-bold">PhotoGallery</span>
+                    </a>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
